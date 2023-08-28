a7a425dabaca3b49434c4443da4592179c870217
anna <anutka2610@gmail.com>
2023-08-28 11:35:24 +0200

    start to work with JS

--- script.js
+++ script.js
@@ -1,52 +1,9 @@
-function changeToogleHover(event) {
-  event.preventDefault();
-  const lightThem = document.querySelector("#lightThem");
-  lightThem.innerHTML = `<img
-                  src="photo/toogle-light-hover.png"
-                  ;
-                  class="img-fluid mx-auto d-block"
-                  alt="toggle to the dark them"
-                />
-              `;
-}
-function changeTooglelight(event) {
-  event.preventDefault();
-
-  const lightThem = document.querySelector("#lightThem");
-  lightThem.innerHTML = `<img
-                  src="photo/toogle-light.png"
-                  ;
-                  class="img-fluid mx-auto d-block"
-                  alt="toggle to the dark them"
-                />
-              `;
+function showData(responce) {
+  console.log(responce.message);
+  
 }
 
-function changeToogleEngHover(event) {
-  event.preventDefault();
-  const enlishLenguage = document.querySelector("#lightEnglich");
-  enlishLenguage.innerHTML = `<img
-                  src="photo/eng-hover.png"
-                  ;
-                  class="img-fluid mx-auto d-block" alt="toggle to the english language"
-              /></a>
-                `;
+function getData() {
+  let apiUrl = "http://35.228.45.65:8000/room_1/";
+  axios.get(apiUrl).then(showData);
 }
-
-function changeToogEnglelight(event) {
-  event.preventDefault();
-  const enlishLenguage = document.querySelector("#lightEnglich");
-  enlishLenguage.innerHTML = `<img
-                  src="photo/eng.png"
-                  ;
-                  class="img-fluid mx-auto d-block" alt="toggle to the english language"
-              /></a>
-                `;
-}
-
-const lightThem = document.querySelector("#lightThem");
-const enlishLenguage = document.querySelector("#lightEnglich");
-lightThem.addEventListener("mouseover", changeToogleHover);
-lightThem.addEventListener("mouseout", changeTooglelight);
-enlishLenguage.addEventListener("mouseover", changeToogleEngHover);
-enlishLenguage.addEventListener("mouseout", changeToogEnglelight);
