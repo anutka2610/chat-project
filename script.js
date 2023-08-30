0772973d119763057bbd9fcb6dd251cbb5bc302c
anna <anutka2610@gmail.com>
2023-08-30 18:20:20 +0200

    added changes

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function showData(responce) {
-  console.log(responce.message);
-  
+function showData(response) {
+  console.log(response);
 }
 
-function getData() {
-  let apiUrl = "http://35.228.45.65:8000/room_1/";
-  axios.get(apiUrl).then(showData);
-}
+let apiUrl = "http://35.228.45.65:8000/room_1/";
+axios.get(apiUrl).then(showData);
