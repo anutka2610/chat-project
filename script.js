6608a93c638c3dcb884eed58f825b17f235c682f
anna <anutka2610@gmail.com>
2023-09-06 15:34:33 +0200

    changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function showData(response) {
   console.log(response);
 }
+const apiUrl = "http://35.228.45.65:8000/room_1/";
 
-let apiUrl = "http://35.228.45.65:8000/room_1/";
-axios.get(apiUrl).then(showData);
+axios
+  .get(apiUrl)
+  .then(showData)
+  .catch((error) => {
+    console.error(error);
+  });
